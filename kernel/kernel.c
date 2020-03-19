37d746c6e66f242f605d9f83f2696e3a3640d24c
David Kim <d@vid.kim>
2020-03-19 17:32:21 -0400

    Printing texts
    kprint("string"); prints at cursor.
    kprint_at("string", col, row); prints at that row and column.
    
    It can handle string going over the max column and prints on the next line. It can also handle the new line character and the carriage return character. For now, the new line character is handled like "\r\n".

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,17 @@
+#include "../drivers/screen.h"
+#include "util.h"
+
 void main() {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    clear_screen();
+
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint(str);
+        kprint("\n");
+    }
+
+    kprint("Testing printing a line that is long enough to cause an overflow to the next line.");   
 }

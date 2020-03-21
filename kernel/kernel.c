1c6e17e3e455b43f1cf6e8626611033a04ab78e1
David Kim <d@vid.kim>
2020-03-20 20:48:58 -0400

    Interrupts, keyboards, and simple libc
    All the basic stuff. Keyboard driver, kmalloc. string and memory manipulation.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,40 @@
+#include "../cpu/isr.h"
 #include "../drivers/screen.h"
-#include "util.h"
+#include "kernel.h"
+#include "../libc/string.h"
+#include "../libc/mem.h"
+#include <stdint.h>
 
-void main() {
-    clear_screen();
+void kernel_main() {
+    isr_install();
+    irq_install();
 
-    /* Fill up the screen */
-    int i = 0;
-    for (i = 0; i < 24; i++) {
-        char str[255];
-        int_to_ascii(i, str);
-        kprint(str);
+    asm("int $2");
+    asm("int $3");
+
+    kprint("Type something, it will go through the kernel\n"
+        "Type END to halt the CPU or PAGE to request a kmalloc()\n> ");
+}
+
+void user_input(char *input) {
+    if (strcmp(input, "END") == 0) {
+        kprint("Stopping the CPU. Bye!\n");
+        asm volatile("hlt");
+    } else if (strcmp(input, "PAGE") == 0) {
+        /* Lesson 22: Code to test kmalloc, the rest is unchanged */
+        uint32_t phys_addr;
+        uint32_t page = kmalloc(1000, 1, &phys_addr);
+        char page_str[16] = "";
+        hex_to_ascii(page, page_str);
+        char phys_str[16] = "";
+        hex_to_ascii(phys_addr, phys_str);
+        kprint("Page: ");
+        kprint(page_str);
+        kprint(", physical address: ");
+        kprint(phys_str);
         kprint("\n");
     }
-
-    kprint("Testing printing a line that is long enough to cause an overflow to the next line.");   
+    kprint("You said: ");
+    kprint(input);
+    kprint("\n> ");
 }
